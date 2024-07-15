1236f3623636e131b4a5873f70d544cd6b466ab9
jonss0777 <jonss0777@gmail.com>
2024-07-15 19:46:48 -0400

    Added comments to functions

--- Week2/is_even.py
+++ Week2/is_even.py
@@ -1,2 +1,8 @@
+# Function tests if the number is even
 def is_even(number: int) -> bool:
-    return number%2;  
+    return number%2== 0
+
+# Function tests if the number is odd
+def is_odd(number:int ) -> bool:
+    return is_even(number)
+
